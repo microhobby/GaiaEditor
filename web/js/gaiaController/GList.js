8063df56c4b00eeb3dad5fcafe7deda4e2a4ef76
Matheus Castello <matheus@castello.eng.br>
2018-03-27 13:48:19 -0400

    web: GList: inherit the font size
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- web/js/gaiaController/GList.js
+++ web/js/gaiaController/GList.js
@@ -142,7 +142,7 @@ function GList(largura, altura, topo, esquerda, visivel)
                         subline = "normal";
                 
                 code =	'\n<div id="GList' + this.Id + '"\n' +
-                                                ' class="badWolf" style="display:' + display + '; position:' + position + '; \n' +
+                                                ' class="badWolf things" style="display:' + display + '; position:' + position + '; \n' +
                                                 ' left: ' + this.L + 'px; top: ' + this.T + 'px; width: ' + width + '; \n' +
                                                 ' height: ' + height + '; padding: ' + this.P + 'px;\n' + 
                                                 ' background-color: ' + this.Cb + '; ' +
@@ -151,18 +151,22 @@ function GList(largura, altura, topo, esquerda, visivel)
                                                 ' border-style: solid;\n' +
                                                 ' border-color: ' + this.Cbb + ';\n' +
                                                 ' border-width: ' + this.B + 'px;\n' +
+                                                ' font-size: ' + this.S + 'px\n' +
                                                 ' -webkit-transform: rotate(' + this.A + 'deg);\n' +
                                                 ' -moz-transform: rotate(' + this.A + 'deg);\n' +
                                                 ' -o-transform: rotate(' + this.A + 'deg);\n' +
                                                 ' -ms-transform: rotate(' + this.A + 'deg);\n' +
                                                 ' transform: rotate(' + this.A + 'deg);\n' +
-                                                'box-sizing: initial; \n' +
+                                                ' box-sizing: initial; \n' +
+                                                ' overflow: hidden;' +
                                                 ' -webkit-box-shadow: 9px 20px 18px ' + this.S + 'px ' + this.Cs + ';\n' +
                                                 ' -moz-box-shadow: 9px 14px 18px ' + this.S + 'px ' + this.Cs + ';\n' +
                                                 ' -o-box-shadow: 9px 14px 18px ' + this.S + 'px ' + this.Cs + ';\n' +
                                                 ' -ms-box-shadow: 9px 14px 18px ' + this.S + 'px ' + this.Cs + ';\n' +
                                                 ' box-shadow: 9px 14px 18px ' + this.S + 'px ' + this.Cs + '; opacity: ' + (this.Opacity / 100) + ';' +
-                                                ' z-index: '+this.Zindex+';" class="things">\n' +
+                                                ' z-index: '+this.Zindex+';' +
+                                                ' color: ' + this.Cf + '; font-size: ' + this.SizeFont + 'px; font-family: ' + this.Font + ';\n' +
+                                                ' font-style: ' + italico + '; font-weight: ' + negrito + '; text-decoration: ' + subline + ';">\n' +
                                                 '<div id="GList' + this.Id + 'Items" class="list-group">' +
                                                         //vai ser o conteudo aqui
                                                 '\n</div></div>\n';
